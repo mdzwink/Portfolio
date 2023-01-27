c454596b14851049c7f59baf646af8c4535b143d
mdzwink <mdzwink@gmail.com>
2023-01-27 00:43:36 -0500

    initial game layout, before true 3D

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Navbar from "../components/Navbar";
 import './main.css';
 
 
@@ -7,36 +6,172 @@ import './main.css';
 const IndexPage = () => {
 
   return (
-    <>
-    <Navbar></Navbar>
     <main>
-        <section className="opening-screen" >
-          <h1>Welcome... buckle up!</h1>
-        </section>
-        <section className="about-me" >
-          <h1>About me</h1>
-          <p>With an unconventional background in carpentry I needed to find out how I would apply my skillsets to my coding work...</p>
-          <p>(down arrow)Keep scrolling to learn about my work(down arrow)</p>
-        </section>
-        <section className="projects" >
-          <div className="static-cube">
-            <div className="anim-cube">
-              <div className="cube text">
-                <h3>Projects</h3>
-
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-    </>
+      <div className="leftspinners" >
+        <div className="llaps"></div>
+        <div className="llapm"></div>
+        <div className="llap"></div>
+      </div>
+      <div className="center-charger">
+        <div className="llac"></div>
+        <div className="welcome-txt">
+          <h1><em>w</em></h1>
+          <h1><em>e</em></h1>
+          <h1><em>l</em></h1>
+          <h1><em>c</em></h1>
+          <h1><em>o</em></h1>
+          <h1><em>m</em></h1>
+          <h1><em>e</em></h1>
+          <h1><em>!</em></h1>
+        </div>
+        <div className="tile-grid">
+          {/* 1x11 */}
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          {/* 2x11 */}
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          {/* 3x11 */}
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          {/* 4x11 */}
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          {/* 5x11 */}
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          {/* 6x11 */}
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="key-tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          {/* 7x11 */}
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          {/* 8x11 */}
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          {/* 9x11 */}
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          {/* 10x11 */}
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          {/* 11x11 */}
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+          <div className="tile"></div>
+        </div>
+      </div>
+      <div className="rightspinners" >
+        <div className="llaps"></div>
+        <div className="llapm"></div>
+        <div className="llap"></div>
+      </div>
+      
+    </main>
   )
 }
 
 
+export default IndexPage
 
 export const Head = () => {
   <title>MZ Portfolio</title>
 }
-
-export default { IndexPage, Head }
